4f005bd418f73b72a6004dafef65eb688ba2a19a
yuyaprograming <ienjoyschoollife@gmail.com>
2020-08-05 17:27:35 +0900

    アラームが聞こえるように。

--- app/src/main/java/jp/techacademy/date/yuuya/taskapp5/InputActivity.kt
+++ app/src/main/java/jp/techacademy/date/yuuya/taskapp5/InputActivity.kt
@@ -124,9 +124,11 @@ class InputActivity : AppCompatActivity() {
 
         val title = title_edit_text.text.toString()
         val content = content_edit_text.text.toString()
+        val category = category_edit_text.text.toString()
 
         mTask!!.title = title
         mTask!!.contents = content
+        mTask!!.category = category
         val calendar = GregorianCalendar(mYear, mMonth, mDay, mHour, mMinute)
         val date = calendar.time
         mTask!!.date = date
--- app/src/main/java/jp/techacademy/date/yuuya/taskapp5/MainActivity.kt
+++ app/src/main/java/jp/techacademy/date/yuuya/taskapp5/MainActivity.kt
@@ -103,4 +103,4 @@ class MainActivity : AppCompatActivity() {
 
         mRealm.close()
     }
-}}
+}
