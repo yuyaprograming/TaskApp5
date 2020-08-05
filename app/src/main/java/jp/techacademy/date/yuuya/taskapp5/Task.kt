0bc96fb78cc8917ca34a2d7dd9213fcd2783c453
yuyaprograming <ienjoyschoollife@gmail.com>
2020-08-05 15:56:11 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/date/yuuya/taskapp5/Task.kt
@@ -0,0 +1,17 @@
+package jp.techacademy.date.yuuya.taskapp5
+
+import java.io.Serializable
+import java.util.Date
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class Task : RealmObject(), Serializable {
+    var title: String = ""      // タイトル
+    var contents: String = ""   // 内容
+    var category: String = ""
+    var date: Date = Date()     // 日時
+
+    // id をプライマリーキーとして設定
+    @PrimaryKey
+    var id: Int = 0
+}
---
+++ app/src/main/java/jp/techacademy/date/yuuya/taskapp5/TaskApp.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.date.yuuya.taskapp5
+
+import android.app.Application
+import io.realm.Realm
+
+class TaskApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
